f723cc4c497deb361b80038ff2268130fdbfa9ee
Won Rhim <wwrk22@gmail.com>
2023-07-20 15:51:46 -0400

    Add validation styling for password - Fixed phone number validation styling to make input box green for valid number.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-const foo = 'foo';
+const password = document.getElementById("user-password");
+const passwordConfirm = document.getElementById("user-password-confirm");
+const passwordErrorMsg = "Passwords do not match.";
+
+function setPasswordMismatch(event) {
+  const pwLength = password.value.length;
+  const pwConfirmLength = passwordConfirm.value.length;
+
+  const pwLengthOk = (password.minLength <= pwLength) && (pwLength <= password.maxLength);
+  const pwConfirmLengthOk = (password.minLength <= pwConfirmLength) && (pwConfirmLength <= password.maxLength);
+
+  if ((pwLengthOk && pwConfirmLengthOk) && (password.value === passwordConfirm.value)) {
+    password.classList = "password-match";
+    passwordConfirm.classList = "password-match";
+  } else {
+    if (password.value !== "" && passwordConfirm.value !== "" && pwLength === pwConfirmLength) {
+      password.classList = "password-mismatch";
+      passwordConfirm.classList = "password-mismatch";
+    } else {
+      password.classList = "";
+      passwordConfirm.classList = "";
+    }
+  }
+}
+
+password.addEventListener("input", setPasswordMismatch);
+passwordConfirm.addEventListener("input", setPasswordMismatch);
